d997d74a6daa40aef0b380762db0eca8f4920294
Djoufson CHE BENE <djouflegran@gmail.com>
2024-09-22 22:14:46 +0100

    add cors support

--- MakeIt/Program.cs
+++ MakeIt/Program.cs
@@ -10,13 +10,17 @@ builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSqlite<MakeItContext>(connectionString);
-
+builder.Services.AddCors(opt => opt.AddDefaultPolicy(x =>
+    x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .AllowAnyOrigin()));
 
 var app = builder.Build();
 
 app.UseSwagger();
 
-// get all todos
+app.UseCors();
+
 app.MapTodoEndpoints();
 
 app.MapRazorPages();
